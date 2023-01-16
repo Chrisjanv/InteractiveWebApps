24c2924265be980f22970d5d8690a97697f0fb95
Andre Visser <chrisjanv@gmail.com>
2023-01-16 15:25:11 +0200

    rainbow done

---
+++ interactiveRainbow/index.js
@@ -0,0 +1,21 @@
+document.getElementById("red").onclick = function() {
+    this.style.backgroundColor = "red";
+}
+document.getElementById("orange").onclick = function () {
+    this.style.backgroundColor = "orange";
+}
+document.getElementById("yellow").onclick = function () {
+    this.style.backgroundColor = "yellow";
+}
+document.getElementById("green").onclick = function () {
+    this.style.backgroundColor = "green";
+}
+document.getElementById("blue").onclick = function () {
+    this.style.backgroundColor = "blue";
+}
+document.getElementById("purple").onclick = function () {
+    this.style.backgroundColor = "purple";
+}
+document.getElementById("violet").onclick = function () {
+    this.style.backgroundColor = "violet";
+}
