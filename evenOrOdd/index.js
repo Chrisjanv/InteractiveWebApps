020b57ad4810dfa502c02b5bbeaba95f4c2f3705
Andre Visser <chrisjanv@gmail.com>
2023-01-16 20:22:29 +0200

    done

--- evenOrOdd/index.js
+++ evenOrOdd/index.js
@@ -1,1 +1,17 @@
 let userNumber = prompt('Please provide a number');
+userNumber = parseInt(userNumber);
+
+function evenOrOdd(num) {
+    if (num%2===0){
+        console.log('Even');
+    }
+    else if (num%2===1){
+        console.log('Odd');
+    }
+    else if (num === null || num === "" || isNaN(num)) {
+        alert("You did not enter a number. Please try again.");
+        location.reload();
+    }
+}
+
+evenOrOdd(userNumber);
