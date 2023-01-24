da29b250404e52f8ae172e4bf22af07f69f48862
Andre Visser <chrisjanv@gmail.com>
2023-01-24 16:57:55 +0200

    Validating user input using while statement

--- Three-Input-Calc/js/ifCalculator.js
+++ Three-Input-Calc/js/ifCalculator.js
@@ -1,8 +1,16 @@
 alert('Please enter two whole numbers greater than 0');
 
 let userNumberOne = prompt('Please enter your first number');
+while (userNumberOne <= 0) {
+    userNumberOne = prompt('Please enter your first number again');
+}
+
 let userMathOperator = prompt('Please enter how the numbers need to be calculated');
+
 let userNumberTwo = prompt('Please enter your second number');
+while (userNumberTwo <= 0) {
+    userNumberTwo = prompt('Please enter your second number again');
+}
 let total = 0;
 
 userNumberOne = parseInt(userNumberOne);
