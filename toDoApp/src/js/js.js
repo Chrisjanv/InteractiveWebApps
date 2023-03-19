5986139c3448c489472650b2e675d79679b2b82f
Andre Visser <chrisjanv@gmail.com>
2023-03-19 10:32:26 +0200

    added class of task in js

---
+++ toDoApp/src/js/js.js
@@ -0,0 +1,9 @@
+class Task {
+    constructor(title, description, date_created, date_due, importance){
+        this.title = title;
+        this.description = description;
+        this.date_created = date_created;
+        this.date_due = date_due;
+        this.importance = importance;
+    }
+};
